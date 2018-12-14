939da9abd05e66fb394bfa09189ba2bf68462dd3
Lorenzo Peppoloni <lorenzo@bluevisionlabs.com>
2018-12-14 11:11:09 +0000

    added everything

--- data.py
+++ data.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 import skimage.io
 from skimage.transform import resize
 from imgaug import augmenters as iaa
+from sklearn.model_selection import train_test_split
+import cv2
+
+from itertools import chain
+from collections import Counter
+
 from tqdm import tqdm
 
 import warnings
@@ -18,39 +24,33 @@ class data_generator:
             random_indexes = np.random.choice(len(dataset_info), batch_size)
             batch_images = np.empty((batch_size, shape[0], shape[1], shape[2]))
             batch_labels = np.zeros((batch_size, 28))
+
             for i, idx in enumerate(random_indexes):
                 image = data_generator.load_image(
-                    dataset_info[idx]['path'], shape)   
+                    dataset_info.iloc[idx]['path'], shape)   
                 if augument:
                     image = data_generator.augment(image)
                 batch_images[i] = image
-                batch_labels[i][dataset_info[idx]['labels']] = 1
+                batch_labels[i][dataset_info.iloc[idx]['target_list']] = 1
             yield batch_images, batch_labels
             
     
     def load_image(path, shape):
-        image_red_ch = skimage.io.imread(path+'_red.png')/255.0
-        image_yellow_ch = skimage.io.imread(path+'_yellow.png')/255.0
-        image_green_ch = skimage.io.imread(path+'_green.png')/255.0
-        image_blue_ch = skimage.io.imread(path+'_blue.png')/255.0
+        colors = ["red","green","blue","yellow"]
+        flags = cv2.IMREAD_GRAYSCALE
+        imgs = []
 
-        image_red_ch += (image_yellow_ch/2).astype(np.uint8) 
-        image_blue_ch += (image_yellow_ch/2).astype(np.uint8)
-
-        image = np.stack((
-            image_red_ch, 
-            image_green_ch, 
-            image_blue_ch
-        ), -1)
-        image = resize(image, (shape[0], shape[1]), mode='reflect')
-        return image
-                
+        for color in colors:
+            img = cv2.imread(path+'_'+color+'.png', flags).astype(np.float32)/255
+            img = cv2.resize(img, (shape[0], shape[1]), cv2.INTER_AREA)
+            imgs.append(img)  
+        return np.stack(imgs, axis=-1)
             
     def augment(image):
         augment_img = iaa.Sequential([
             iaa.OneOf([
                 iaa.Affine(rotate=0),
-                iaa.Affine(rotate=90),
+                iaa.Affine(rotate=30),
                 iaa.Affine(rotate=180),
                 iaa.Affine(rotate=270),
                 iaa.Fliplr(0.5),
@@ -59,3 +59,32 @@ class data_generator:
         
         image_aug = augment_img.augment_image(image)
         return image_aug
+
+
+def get_data(image_folder, csv_path):
+    image_df = pd.read_csv(os.path.join(csv_path))
+    print(image_df.shape[0], 'masks found')
+    print(image_df['Id'].value_counts().shape[0])
+    # just use green for now
+
+    image_df['path'] = image_df['Id'].map(lambda x: os.path.join(image_folder, x))
+    image_df['target_list'] = image_df['Target'].map(lambda x: [int(a) for a in x.split(' ')])
+    
+    all_labels = list(chain.from_iterable(image_df['target_list'].values))
+    c_val = Counter(all_labels)
+    n_keys = c_val.keys()
+    max_idx = max(n_keys)
+
+    image_df['target_vec'] = image_df['target_list'].map(lambda ck: [i in ck for i in range(max_idx+1)])
+
+    return image_df
+
+def split_data(data):
+    raw_train_df, valid_df = train_test_split(data, 
+                 test_size = 0.3, 
+                  # hack to make stratification work                  
+                 stratify = data['Target'].map(lambda x: x[:3] if '27' not in x else '0'))
+    print(raw_train_df.shape[0], 'training masks')
+    print(valid_df.shape[0], 'validation masks')
+
+    return raw_train_df, valid_df
--- model.py
+++ model.py
@@ -15,13 +15,18 @@ from keras.callbacks import ReduceLROnPlateau
 from keras import metrics
 from keras.optimizers import Adam 
 from keras import backend as K
+from sklearn.metrics import f1_score
+from skmultilearn.model_selection import iterative_train_test_split
+
+
 import keras
 import tensorflow as tf
 
 import pandas as pd
 
-from data import data_generator
+from data import data_generator, get_data, split_data
 
+np.random.seed(2018)
 
 def f1(y_true, y_pred):
     '''
@@ -59,37 +64,17 @@ def f1(y_true, y_pred):
     recall = recall(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-def f1_loss(y_true, y_pred):
-    
-    #y_pred = K.cast(K.greater(K.clip(y_pred, 0, 1), THRESHOLD), K.floatx())
-    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
-    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
-    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
-    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
-
-    p = tp / (tp + fp + K.epsilon())
-    r = tp / (tp + fn + K.epsilon())
-
-    f1 = 2*p*r / (p+r+K.epsilon())
-    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
-    return 1-K.mean(f1)
-
-
-def reduce(x):
-    return K.argmax(x, axis=1)
-
-def cast(x):
-    return K.cast(x, 'float32')
 
 def create_model(input_shape, n_out):
     inp = Input(input_shape)
     pretrain_model = MobileNetV2(include_top=False, weights=None, input_tensor=inp)
-    #x = pretrain_model.get_layer(name="block_13_expand_relu").output
     x = pretrain_model.output
     
     x = GlobalAveragePooling2D()(x)
     x = Dropout(0.5)(x)
-    x = Dense(n_out, activation="relu")(x)
+    # x = Dense(n_out, activation="sigmoid")(x)
+    # We use linear for focal loss, which already contains a sigmoid.
+    x = Dense(n_out, activation="linear")(x)
     
     for layer in pretrain_model.layers:
         layer.trainable = True
@@ -99,50 +84,31 @@ def create_model(input_shape, n_out):
 @click.command(help="Create dataset.")
 @click.option("-i", "--images-path", prompt=True, type=str)
 @click.option("-l", "--labels-path", prompt=True, type=str)
-def main(images_path, labels_path)
+def main(images_path, labels_path):
     keras.backend.clear_session()
-    data = pd.read_csv(labels_path)
 
-    train_dataset_info = []
+    data_df = get_data(images_path, labels_path)
 
-    for name, labels in zip(data['Id'], data['Target'].str.split(' ')):
-        train_dataset_info.append({
-            'path':os.path.join(images_path, name),
-            'labels':np.array([int(label) for label in labels])})
-    train_dataset_info = np.array(train_dataset_info)
+    raw_train, valid =  split_data(data_df)
 
-
-    input_shape = (256,256,3)
-
-    # create train datagen
-    train_datagen = data_generator.create_train(
-        train_dataset_info, 5, input_shape, augument=True)
-
-    images, labels = next(train_datagen)
-
-    print('min: {0}, max: {1}'.format(images.min(), images.max()))
+    input_shape = (256,256,4)
 
     model = create_model(input_shape=input_shape, n_out=28)
-    model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['acc', f1])
+    # model.compile(loss="binary_crossentropy", optimizer=Adam(), metrics=['acc', f1])
+    model.compile(loss=[_focal_loss(gamma=2,alpha=0.75)], optimizer=Adam(), metrics=['acc', f1])
 
-    epochs = 5; batch_size = 64
+    epochs = 50
+    batch_size = 64
     checkpointer = ModelCheckpoint('../working/InceptionResNetV2.model', verbose=2, save_best_only=True)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=1, factor=0.1)
+    early_stopping = EarlyStopping(monitor="val_loss", patience=2)
+    reduce_lr = ReduceLROnPlateau(monitor="val_loss", patience=1, factor=0.1)
+    
+    train_generator = data_generator.create_train(raw_train, batch_size, input_shape, augument=True)
+    validation_generator = data_generator.create_train(valid, 100, input_shape, augument=False)
 
-    # split and suffle data 
-    np.random.seed(2018)
-    indexes = np.arange(train_dataset_info.shape[0])
-    np.random.shuffle(indexes)
-    train_indexes = indexes[:27500]
-    valid_indexes = indexes[27500:]
+    train_steps = raw_train.shape[0]//batch_size
+    valid_steps = valid.shape[0]//batch_size
 
-    train_steps = len(train_indexes)//batch_size
-    valid_steps = len(valid_indexes)//batch_size
-
-    # create train and valid datagens
-    train_generator = data_generator.create_train(train_dataset_info[train_indexes], batch_size, input_shape, augument=True)
-    validation_generator = data_generator.create_train(train_dataset_info[valid_indexes], 100, input_shape, augument=False)
 
     # train model
     history = model.fit_generator(
@@ -153,6 +119,14 @@ def main(images_path, labels_path)
         epochs=epochs, 
         verbose=1,
         callbacks=[checkpointer, reduce_lr])
+
+
+def _focal_loss(gamma=2, alpha=0.75):
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+    return focal_loss_fixed
 
 
 if __name__ == "__main__":
